0e5b855dc7494e5a756357d2c16fc2bd267576b2
Chandramohan2727 <Undra.Mohan@Triconinfotech.com>
2025-11-05 16:31:36 +0530

    Create q5.sql

---
+++ q5.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (department_id, department_name) VALUES
+(1, 'Human Resources'),
+(2, 'Finance'),
+(3, 'IT'),
+(4, 'Marketing');
+
+INSERT INTO employee (employee_id, employee_name, salary) VALUES
+(101, 'John Doe',  45000),
+(102, 'Alice Smith',  55000),
+(103, 'Michael Johnson',  60000),
+(104, 'Emma Brown',  50000);
